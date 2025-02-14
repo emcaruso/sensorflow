fceb3932e03597adada7630cf00935264a6066b7
emcaruso <emcaruso@unibz.it>
2025-02-14 18:34:24 +0100

    instant save

--- src/collector.py
+++ src/collector.py
@@ -1,4 +1,5 @@
 import os, sys
+from typing import List
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 from shutil import rmtree
 from pathlib import Path
@@ -53,7 +54,33 @@ class Collector():
                     self.logger.warning(f"Light on took {delta} seconds, more than the maximum interval: {interval}")
                 time.sleep(self.period - delta)
 
-    def capture_light_sequence(self):
+    def __collect(self, images : List[Image], images_show : List[Image], in_ram : bool = False):
+
+        out_dir = Path(self.cfg.paths.save_dir)
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
+
+        if in_ram:
+            self.__images_list.append(images)
+            self.__images_postprocessed_list.append(images_show)
+        else:
+            self.__save(images, raw = True)
+            self.__save(images_show, raw = False)
+
+        self.logger.info(f"Images captured (total: {self.__counter} per cam)")
+
+        self.__counter += 1
+
+    def __collect_init(self):
+        self.__images_list = []
+        self.__images_postprocessed_list = []
+        self.__counter = 0
+        rmtree(self.cfg.paths.save_dir, ignore_errors=True)
+        os.makedirs(self.cfg.paths.save_dir)
+
+    def capture_light_sequence(self, in_ram : bool = False, show : bool = False):
+        self.__collect_init()
+
         if self.collection_cfg is None:
             error_msg = "Not able to collect light sequence: Collection config not found"
             self.logger.error(error_msg)
@@ -64,50 +91,53 @@ class Collector():
         p = mp.Process(target=self.__led_sequence_updater, args=[])
         p.start()
         self.cam_controller.grab_images() # remove first image from buffer
-        images_list = []
-        images_postprocessed_list = []
         for _ in range(self.collection_cfg.light_sequence.rounds + len(self.collection_cfg.light_sequence.sequence)):
             images = self.cam_controller.grab_images()
-            images_list.append(images)
             images_postprocessed = self.postprocessing.postprocess(images)
-            images_postprocessed_list.append(images_postprocessed)
-        for images in images_list:
-            Image.show_multiple_images(images_postprocessed, wk = 0)
+            self.__collect(images, images_postprocessed, in_ram)
+            if show:
+                Image.show_multiple_images(images_postprocessed, wk = 0)
         self.cam_controller.stop_cameras()
-        return images_list
 
-    def capture_manual(self):
+    def capture_manual(self, in_ram : bool = False):
+        self.__collect_init()
+
         self.cam_controller.start_cameras_synchronous_latest()
-        images_list = []
-        images_postprocessed_list = []
         while True:
             images = self.cam_controller.grab_images()
             images_postprocessed = self.postprocessing.postprocess(images)
-            key = Image.show_multiple_images(images, wk = 1)
+            key = Image.show_multiple_images(images_postprocessed, wk = 1)
             if key == ord('q'):
                 break
             if key == 32:
-                self.logger.info(f"Images captured (total: {len(images_list)} per cam)")
-                images_list.append(images)
-                images_postprocessed_list.append(images_postprocessed)
-        return images_list, images_postprocessed_list
+                self.__collect(images, images_postprocessed, in_ram)
 
-    def save(self, images_list) -> bool:
-        if images_list == []:
-            self.logger.info(f"Not saving because no images are captured")
-            return False
+    def capture_n_images(self, n : int, in_ram : bool = False, show : bool = False):
 
-        rmtree(self.cfg.paths.save_dir, ignore_errors=True)
-        os.makedirs(self.cfg.paths.save_dir)
+        # Preliminary
+        while True:
+            images = self.cam_controller.grab_images()
+            images_postprocessed = self.postprocessing.postprocess(images)
+            key = Image.show_multiple_images(images_postprocessed, wk = 1)
+            if key == 32:
+                break
+            
+        # Collect
+        for _ in range(n):
+            images = self.cam_controller.grab_images()
+            images_postprocessed = self.postprocessing.postprocess(images)
+            self.__collect(images, images_postprocessed, in_ram)
 
-        # save data
-        for i, images in enumerate(images_list):
-            img_name = "img_" + str(i).zfill(3)
+    def save(self, save_raw : bool = False, save_postprocessed : bool = True, verbose = True) -> bool:
 
-            # save images
-            for j, image in enumerate(images):
-                cam_name = "cam_" + str(j).zfill(3)
-                image.save(str(Path(self.cfg.paths.save_dir) / f"{cam_name}" / f"{img_name}.png"))
+        # save images (if any)
+        self.__counter = 0
+        for i in range(len(self.__images_list)):
+            if save_raw:
+                self.__save(self.__images_list[i], raw = True, verbose = verbose)
+            if save_postprocessed:
+                self.__save(self.__images_postprocessed_list[i], raw = False, verbose = verbose)
+            self.__counter += 1
 
         # save devices info
         devices_info = self.cam_controller.get_devices_info()
@@ -120,6 +150,18 @@ class Collector():
             with open(str(Path(self.cfg.paths.save_dir) / "collection_cfg.yaml"), 'w') as f:
                 omegaconf.OmegaConf.save(self.collection_cfg, f)
             self.logger.info(f"Collection config saved in {self.cfg.paths.save_dir}")
-         
+
         return True
+
+    def __save(self, images : List[Image], raw : bool, verbose : bool = False):
+        subdir = "raw" if raw else "postprocessed"
+        img_name = str(self.__counter).zfill(3) + ".png"
+        if images is not None:
+            for cam_id in range(len(images)):
+                cam_name = "cam_" + str(cam_id).zfill(3)
+                image = images[cam_id]
+                o_dir = Path(self.cfg.paths.save_dir) / subdir / cam_name 
+                if not os.path.exists(o_dir):
+                    os.makedirs(o_dir)
+                image.save_parallel(o_dir / img_name, verbose = verbose)
 
